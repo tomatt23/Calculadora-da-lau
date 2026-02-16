06e6296434dec4fe41673606768b0d9dc438de01
tomatt23 <ma2037051@gmail.com>
2026-02-16 02:07:36 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function insertToDisplay(data) {
+  document.querySelector('#display').value += data
+}
+
+function clean() {
+  document.querySelector('#display').value = 0
+}
+
+function backspace() {
+  const display = document.querySelector('#display')
+  display.value = display.value.slice(0, -1)
+}
+
+function resultado() {
+  const display = document.querySelector('#display')
+  try {
+    display.value = eval(display.value)
+  } catch {
+    display.value = 'Erro'
+  }
+}
